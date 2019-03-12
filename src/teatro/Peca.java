3418cdd233c0594c3d7334359bde0e5182d9ce72
FabioAJ <fabioaugusto.aj@gmail.com>
2019-03-12 15:22:29 -0300

    Implementa metodos basicos da classe Peca

--- src/teatro/Peca.java
+++ src/teatro/Peca.java
@@ -1,5 +1,97 @@
 package teatro;
 
-public class Peca {
+import java.io.Serializable;
+import java.util.Date;
 
+public class Peca implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private Integer ID;
+	private String titulo;
+	private Integer duracao;
+	private Date data;
+	
+	public Peca(Integer iD, String titulo, Integer duracao, Date data) {
+		super();
+		ID = iD;
+		this.titulo = titulo;
+		this.duracao = duracao;
+		this.data = data;
+	}
+
+	public Integer getID() {
+		return ID;
+	}
+
+	public void setID(Integer iD) {
+		ID = iD;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Integer getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(Integer duracao) {
+		this.duracao = duracao;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ID == null) ? 0 : ID.hashCode());
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((duracao == null) ? 0 : duracao.hashCode());
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Peca other = (Peca) obj;
+		if (ID == null) {
+			if (other.ID != null)
+				return false;
+		} else if (!ID.equals(other.ID))
+			return false;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (duracao == null) {
+			if (other.duracao != null)
+				return false;
+		} else if (!duracao.equals(other.duracao))
+			return false;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		return true;
+	}
+	
 }
